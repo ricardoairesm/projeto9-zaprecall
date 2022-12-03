599c6356f08e784102602593c74bf14393a2a355
ricardoairesm <ricardoaamb@usp.br>
2022-12-03 13:28:37 -0300

    tela inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styled from 'styled-components';
+import logo from './assets/img/logo.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(props) {
+    const [disabled, setDisabled] = useState("flex");
+    return (
+        <Corpo>
+            <Logo disabled={disabled}>
+                <img src={logo} />
+            </Logo>
+            <Botao onClick = {trocarTela} disabled = {disabled}>Iniciar Recall!</Botao>
+        </Corpo>
+    )
+
+    function trocarTela() {
+        setDisabled("none");
+    }
 }
 
-export default App;
+const Corpo = styled.div`
+
+box-sizing: border-box;
+position: relative;
+width: 375px;
+height: 667px;
+background: #FB6B6B;
+border: 1px solid #DBDBDB;
+
+`
+const Logo = styled.div`
+position: absolute;
+width: 136px;
+height: 161px;
+left:119px;
+top:148px;
+display: ${props => props.disabled};
+`
+
+const Botao = styled.div`
+position: absolute;
+width: 246px;
+height: 54px;
+top:411px;
+left:64px;
+background: #FFFFFF;
+border: 1px solid #D70900;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+border-radius: 5px;
+display:${props => props.disabled};
+align-items:center;
+justify-content:center;
+font-family: 'Recursive';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 22px;
+color:#D70900;
+`
